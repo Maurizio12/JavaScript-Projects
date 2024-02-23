8f97e4561b9278af8bc5e4cf337fb4af70880923
Maurizio12 <mauriziogizzi12@gmail.com>
2024-02-23 16:55:14 +0000

    upafte main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+window.alert("Hello Word");
+
+document.write("Hello Word");
+
+var A = " This is a string";
+document.write(A);
+
+var name =" My name is Ken "
+window.alert(name)
+
+var car ="Ferrari"
+document.write(" My car is a " + car);
+
+var solarSistem =" milkyWay ", Jupiter = " redOne ", Saturn =" rings "
+    Pluto =" farest ", Venus =" beauty "
+document.write(Pluto);
