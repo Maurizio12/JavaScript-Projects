2b4d08f7cada6e17be8281a9c28d42172a671f09
Maurizio12 <mauriziogizzi12@gmail.com>
2024-02-23 17:29:01 +0000

    upadte

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,16 +1,19 @@
-window.alert("Hello Word");
+window.alert("Hello Word"); //windows.alert method
 
-document.write("Hello Word");
+document.write("Hello Word"); //document.write method
 
-var A = " This is a string";
+var A = " This is a string"; //defining a variable with a string value
 document.write(A);
 
-var name =" My name is Ken "
+var name =" My name is Ken " //defining a variable with a string value
 window.alert(name)
 
 var car ="Ferrari"
 document.write(" My car is a " + car);
 
-var solarSistem =" milkyWay ", Jupiter = " redOne ", Saturn =" rings "
+var solarSistem =" milkyWay ", Jupiter = " redOne ", Saturn =" rings " //defining multiple variables with a string values
     Pluto =" farest ", Venus =" beauty "
-document.write(Pluto);
+document.write(Pluto); //display the variable pluto only 
+
+3+3  //expression
+document.write(3+3);
