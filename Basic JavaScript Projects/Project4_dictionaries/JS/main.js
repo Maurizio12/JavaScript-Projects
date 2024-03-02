50be585d70630e891aa63c60cc38f50de51c2c69
Maurizio12 <mauriziogizzi12@gmail.com>
2024-03-02 15:00:05 +0000

    dictionary created

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,12 @@
+//Dictionary
+function my_Dictionary (){
+    var Animal = {
+        species: "Dog",
+        Color: "black",
+        Breed: "Labrador",
+        Age: 5,
+        Sound: "Bark"
+    };
+    delete Animal.Color;                                             // if I chose color will be undefined 
+    document.getElementById("Dictionary").innerHTML = Animal.Color;  //Animal.Breed parents.child 
+}
