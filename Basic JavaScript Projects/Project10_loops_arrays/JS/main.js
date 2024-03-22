57b057ff95229b595a6199afa6432e1ff4840fab
Maurizio12 <mauriziogizzi12@gmail.com>
2024-03-22 17:48:24 +0000

    update

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,1 +1,8 @@
-alert ("hello");
+function call_Loop () {
+    var loops = "" ;
+    var X = 1;
+    while (X < 10) {
+        loops += +X; X++;
+    }       
+    document.getElementById ("loop").innerHTML = loops;
+}
